ddc698a8e24b0d27b27d523f46123336467e648b
Will Vining <awvining91@gmail.com>
2022-09-03 22:11:39 -0700

    edited javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,19 @@
 
+var citiesListArr = [];
+var numOfCities = 9;
+
+var unit = "units=imperial";
+
+var dailyUVIndexApiStarts = "https://api.openweathermap.org/data/2.5/uvi?";
+var forecastWeatherApiStarts =
+  "https://api.openweathermap.org/data/2.5/onecall?";
+// select from html element
+var searchCityForm = $("#searchCityForm");
+var searchedCities = $("#searchedCityLi");
+
+
+
+
 
 var dailyWeatherApiStarts = "https://api.openweathermap.org/data/3?q=?"
 var personAPIKey = "appid=166ce9dbf117f228937f391618d752ca";
@@ -7,7 +22,7 @@ var getCityWeather = function (searchCityName) {
      var apiUrl =
      dailyWeatherApiStarts + searchCityName + "&" + unit;
 
-    fetch(apiUrl).then(function (response)) {
+    fetch(apiUrl).then(function (response) {
         if (response.ok ) {
             return response.json().then(function (response) {
                 $("#cityName").html(response.name);
@@ -23,6 +38,21 @@ var getCityWeather = function (searchCityName) {
 
                 $("#weatherIconToday").attr("src", weatherIncoUrl);
                 $("tempToday").html(response.main.temp + " \u00B0F");
+                $("#windSpeedToday").html(response.wind.speed + " MPH");
+
+                var lat = response.coord.lat;
+                var lon = reponse.coord.lon;
+                getUVIndex(lat, lon);
+                getForecast(lat, lon);
+            });
+        } else {
+            alert("Please provide a valid city name.");
+        }
+    });
+};
+
+       
+
                 
 
 
@@ -32,7 +62,6 @@ var getCityWeather = function (searchCityName) {
 
 
 
-            }
 
 
 
@@ -40,41 +69,3 @@ var getCityWeather = function (searchCityName) {
 
 
 
-
-
-        }
-
-
-
-
-
-    }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-}
