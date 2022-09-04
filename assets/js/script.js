9a30a8462828f40b8078dc05da1f81b9718877a0
Will Vining <awvining91@gmail.com>
2022-09-03 21:25:23 -0700

    working on starting javascript

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+
+
+var dailyWeatherApiStarts = "https://api.openweathermap.org/data/3?q=?"
+var personAPIKey = http://api.openweathermap.org/data/2.5/forecast?id=524901&appid={166ce9dbf117f228937f391618d752ca}
+
+var getCityWeather = function (searchCityName) {
+     var apiUrl =
+     dailyWeatherApiStarts + searchCityName + "&" + unit;
+
+
+
+
+
+}
