f4c111d5725a9782785bab1b31a0917f7c66e36d
Will Vining <awvining91@gmail.com>
2022-09-03 21:54:28 -0700

    working on javascript fetch response

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,77 @@
 
 
 var dailyWeatherApiStarts = "https://api.openweathermap.org/data/3?q=?"
-var personAPIKey = http://api.openweathermap.org/data/2.5/forecast?id=524901&appid={166ce9dbf117f228937f391618d752ca}
+var personAPIKey = "appid=166ce9dbf117f228937f391618d752ca";
 
 var getCityWeather = function (searchCityName) {
      var apiUrl =
      dailyWeatherApiStarts + searchCityName + "&" + unit;
+
+    fetch(apiUrl).then(function (response)) {
+        if (response.ok ) {
+            return response.json().then(function (response) {
+                $("#cityName").html(response.name);
+
+                var unixTime = response.dt;
+                var date = moment.unix(unixTime).format("MM/DD/YY");
+                $("#currentdate").html(date);
+
+                var weatherIncoUrl =
+                    "http://openweatherapp.org/img/wn" +
+                    response.weather[0].icon +
+                    "@2x.png";
+
+                $("#weatherIconToday").attr("src", weatherIncoUrl);
+                $("tempToday").html(response.main.temp + " \u00B0F");
+                
+
+
+
+
+
+
+
+
+            }
+
+
+
+
+
+
+
+
+
+        }
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
